1e206f761c5e7128aef15e51ef531678425a7ba9
Hadsan Geele <hgeele22@gmail.com>
2018-04-16 20:40:55 +0100

    Added CSS

--- app.rb
+++ app.rb
@@ -3,6 +3,12 @@ require 'sinatra/contrib/all' if development?
 also_reload("./models")
 require_relative('./models/game.rb')
 
+
+get "/" do
+erb(:home)
+end
+
+
 get "/:hand1/:hand2" do
 @result = Game.compare(params[:hand1], params[:hand2])
 erb(:result)
