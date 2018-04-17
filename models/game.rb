c6fa7efdb4854797d4ef524b7a542265c3ce6e05
Hadsan Geele <hgeele22@gmail.com>
2018-04-17 08:26:23 +0100

    changes in game page

--- models/game.rb
+++ models/game.rb
@@ -5,11 +5,11 @@ class Game
     elsif (hand1 == "rock" && hand2 == "scissors") ||
       (hand1 == "scissors" && hand2 == "paper") ||
       (hand1 == "paper" && hand2 == "rock")
-      return "Hand 1 wins!"
+      return "Hand 1!"
     elsif (hand1 == "rock" && hand2 == "paper") ||
       (hand1 == "scissors" && hand2 == "rock") ||
       (hand1 == "paper" && hand2 == "scissors")
-      return "Hand 2 wins!"
+      return "Hand 2!"
     end
   end
 
